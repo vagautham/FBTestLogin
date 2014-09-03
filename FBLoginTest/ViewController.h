e615a785cd60c50932a7221384fb64f8387877be
VA Gautham <ivagautham@gmail.com>
2014-09-03 13:48:08 -0400

    Initial Commit

---
+++ FBLoginTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FBLoginTest
+//
+//  Created by VA Gautham  on 9/3/14.
+//  Copyright (c) 2014 Gautham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FBLoginTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FBLoginTest
+//
+//  Created by VA Gautham  on 9/3/14.
+//  Copyright (c) 2014 Gautham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
